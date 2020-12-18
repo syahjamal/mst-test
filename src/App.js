fb6d913e55be0d611a4063408465cf789d157f7e
syahjamal <sulaimansyahjamal@gmail.com>
2020-12-18 20:50:51 +0700

    clean code

--- src/App.js
+++ src/App.js
@@ -6,12 +6,6 @@ import CircularProgress from "@material-ui/core/CircularProgress"
 import mst from './images/mst.jpg';
 import './App.css';
 
-// function sleep(delay = 0){
-//   return new Promise(resolve => {
-//     setTimeout(resolve, delay)
-//   })
-// }
-
 
 function App() {
   // Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
